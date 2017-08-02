c46d2dcbd4debd4c18a9ffaf46eb13a0d900bd77
Jack <lifedna.jack@gmail.com>
2017-08-02 21:00:45 +0800

    Bind permission on create product

--- estore/views.py
+++ estore/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import redirect, render
 
 from .forms import ProductForm
@@ -9,7 +10,7 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'estore/product_list.html', {'products': products})
 
-
+@permission_required('estore.add_product')
 def product_create(request):
     if request.method == "POST":
         form = ProductForm(request.POST)
@@ -19,3 +20,6 @@ def product_create(request):
     else:
         form = ProductForm()
     return render(request, 'estore/product_form.html', {'form': form})
+
+
+
