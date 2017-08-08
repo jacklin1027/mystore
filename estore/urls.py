40a4f254f7a199af4163ebb714447ad1866bcef0
Jack <lifedna.jack@gmail.com>
2017-08-08 14:19:11 +0800

    Heroku support

--- estore/urls.py
+++ estore/urls.py
@@ -6,7 +6,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ProductList.as_view(), name='product_list'),
-    url(r'^create$', views.product_create, name='product_create'),
     url(r'^(?P<pk>\d+)/$', views.ProductDetail.as_view(), name='product_detail'),
 
     url(r'^dashboard/products/$', views.ProductList.as_view(template_name='estore/dashboard_product_list.html', permission_required='estore.change_product'), name='dashboard_product_list'),
